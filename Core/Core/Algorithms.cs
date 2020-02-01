a7d7bb864994583795fbdca744f0728f60a92cc1
Jose Solano R <josesolano@outlook.com>
2020-02-01 18:25:38 -0400

    creation of the basic classes of logic

---
+++ Core/Core/Algorithms.cs
@@ -0,0 +1,30 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Core
+{
+    public class Algorithms : IAlgorithms
+    {
+        public bool BestFit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FirstFit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NextFit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WorstFit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/Core/Repository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Interfaces;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Core
+{
+    public class Repository : IRepository
+    {
+        public Task<bool> AddProcessByAlgorithm(ProcessDTO processData, string algorithmType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BlockDTO> GetAllMemoryBlocks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
